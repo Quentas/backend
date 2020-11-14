1995c6b09deb3dcbbe8752cefd4f5a9c66de6c0e
Quentas <quentas320@gmail.com>
2020-11-15 00:37:04 +0200

    db changes

--- backend/settings.py
+++ backend/settings.py
@@ -80,12 +80,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'proj_db',
-        'USER' : 'postgres',
-        'PASSWORD' : 'B1234_1',
-        'HOST' : 'localhost',
-        'PORT': '5432',
+
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+ #       'NAME': 'proj_db',
+  #      'USER' : 'postgres',
+   #     'PASSWORD' : 'B1234_1',
+    #    'HOST' : 'localhost',
+     #   'PORT': '5432',
+      'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
 

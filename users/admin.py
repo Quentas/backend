b990b483cafe6675523db3c7b497917f92c5f1a7
Quentas <quentas320@gmail.com>
2021-08-24 22:39:36 +0300

    outstanding token deleting bug fix

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from rest_framework_simplejwt import token_blacklist
+
 from .models import (
     Account,
     Post,
@@ -7,7 +9,18 @@ from .models import (
 )
 
 
+class OutstandingTokenAdmin(token_blacklist.admin.OutstandingTokenAdmin):
+
+    def has_delete_permission(self, *args, **kwargs):
+        return True 
+
+admin.site.unregister(token_blacklist.models.OutstandingToken)
+admin.site.register(token_blacklist.models.OutstandingToken, OutstandingTokenAdmin)
+
+
 admin.site.register(Account)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Picture)
+
+
